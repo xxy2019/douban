be14b076093c0a0c1b6e96b6e63a3a9bf9ed5647
xxy2019 <3314361808@qq.com>
2019-11-13 20:08:45 +0800

    豆瓣代理与接口重写

--- config/index.js
+++ config/index.js
@@ -10,7 +10,15 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      '/api': {
+        target: 'http://106.13.226.142:8091',  //目标接口域名
+        changeOrigin: true,  //是否跨域
+        pathRewrite: {
+          '^/api': '/api'   //重写接口
+        }
+      }
+    },
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST

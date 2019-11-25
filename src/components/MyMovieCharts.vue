b09b460774ecf004df42723eb055dc04015628dc
xiang-ops <xiang_x1207@163.com>
2019-11-25 19:56:41 +0800

    no message

--- src/components/MyMovieCharts.vue
+++ src/components/MyMovieCharts.vue
@@ -1,22 +1,40 @@
 <template>
     <div>
         <List>
-            <ListItem class='listitem' v-for="(item,index) in content" :key="item.index">
+            <ListItem class='listitem' v-for="(item,index) in content" :key="index">
                 <Col :xs="24" :sm="12" :md="12" :lg="12" style="font-size:0.23rem">
-                    {{index+1+'、'+item.name+" by"+item.director}}
+                  <Icon type="ios-ribbon-outline"  :class="JudgeClass[index]" v-if='index<3'/>
+<span v-if='index>2'>{{index+1+"、"}}</span>{{item.name+" by"+item.director}}
                 </Col>
             </ListItem>
         </List>
     </div>
 </template>
 <script>
-    export default {
-        name:'MyMovieCharts',
-        props:['content']
+export default {
+  name: 'MyMovieCharts',
+  props: ['content'],
+  data () {
+    return {
+      JudgeClass: ['gold', 'silver', 'copper']
     }
+  }
+}
 </script>
 <style scoped>
 .listitem{
     border: none;
+}
+.gold{
+   font-size:.325rem;
+   color:gold;
+}
+.silver{
+   font-size:.325rem;
+   color:silver;
+}
+.copper{
+   font-size:.325rem;
+   color:#b87333 ;
 }
 </style>
